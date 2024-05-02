85b5ae5a1c2727094aa5e3897308af36a48ea94e
saltchicken <john.eicher89@gmail.com>
2024-05-01 18:15:15 -0700

    Get first argument instead of explicit -i

--- spritemaker/__init__.py
+++ spritemaker/__init__.py
@@ -3,7 +3,7 @@ from .classes import Spritemaker
 
 def main():
     parser = argparse.ArgumentParser(description="Spritemaker commandline")
-    parser.add_argument('-i', '--input', required=True, help='Input image')
+    parser.add_argument('input', required=True, help='Input image')
     args = parser.parse_args()
     spritemaker = Spritemaker(args.input)
     spritemaker.show()
