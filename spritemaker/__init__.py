b3ccc7bec936a069362280e332b685ed19c51687
saltchicken <john.eicher89@gmail.com>
2024-05-01 18:10:22 -0700

    Fix

--- spritemaker/__init__.py
+++ spritemaker/__init__.py
@@ -1,1 +1,10 @@
 from .classes import Spritemaker
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Spritemaker commandline")
+    parser.add_argument('-i', '--input', required=True, help='Input image')
+    args = parser.parse_args()
+    spritemaker = Spritemaker(args.input)
+    spritemaker.show()
