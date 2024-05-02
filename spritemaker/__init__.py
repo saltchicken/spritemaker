e666719da2572fa1d4fae789108f23673e79fab3
saltchicken <john.eicher89@gmail.com>
2024-05-01 18:17:12 -0700

    Go to current working directory

--- spritemaker/__init__.py
+++ spritemaker/__init__.py
@@ -1,9 +1,10 @@
-import argparse
+import argparse, os
 from .classes import Spritemaker
 
 def main():
     parser = argparse.ArgumentParser(description="Spritemaker commandline")
     parser.add_argument('input', help='Input image')
     args = parser.parse_args()
+    os.chdir(os.getcwd())
     spritemaker = Spritemaker(args.input)
     spritemaker.show()
