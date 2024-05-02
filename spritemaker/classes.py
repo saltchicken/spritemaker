820fac595d962a061d9293debea2e580b200a1af
saltchicken <john.eicher89@gmail.com>
2024-05-01 20:55:15 -0700

    Add ability to extract sprites

--- spritemaker/classes.py
+++ spritemaker/classes.py
@@ -9,6 +9,7 @@ class Spritemaker():
         self.image = self.read_image(image_path)
         self.image = self.remove_background(self.image)
         self.rectangles = self.extract_rectangles(self.image)
+        self.sprites = []
     
     def read_image(self, image_path):
         image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
@@ -26,7 +27,11 @@ class Spritemaker():
         background_mask = cv2.inRange(image, lower_alpha, upper_alpha)
         contours, _ = cv2.findContours(background_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        return [cv2.boundingRect(contour) for contour in contours]
+        rectangles = [cv2.boundingRect(contour) for contour in contours]
+        
+        # Sort the rectangles from left to right
+        rectangles = sorted(rectangles, key=lambda x: x[0])
+        return rectangles
 
     def show_bounding_boxes(self, image, rectangles):
         for rect in rectangles:
@@ -34,10 +39,29 @@ class Spritemaker():
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0, 255), 2)
         plt.imshow(image)
         plt.show()
+
+    def image_from_rect(self, rect):
+        x, y, w, h = rect
+        return self.image[y:y+h, x:x+w]
+    
+    def extract_sprites(self):
+        for rect in self.rectangles:
+            self.sprites.append(self.image_from_rect(rect))
         
     def show(self):
         self.show_bounding_boxes(self.image, self.rectangles)
+    
+    def show_sprite(self, index):
+        # self.show_bounding_boxes(self.sprites[index], self.rectangles[index])
+        plt.imshow(self.sprites[index])
+        plt.show()
         
 if __name__ == "__main__":
-    spritemaker = Spritemaker('attack_1.png')
-    spritemaker.show()
+    spritemaker = Spritemaker('FSS.png')
+    # spritemaker.show()
+    spritemaker.extract_sprites()
+    # spritemaker.show_sprite(0)
+    # spritemaker.show_sprite(1)
+    # spritemaker.show_sprite(2)
+    # spritemaker.show_sprite(3)
+    
