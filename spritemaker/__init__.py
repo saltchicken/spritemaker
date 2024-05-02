0fb06cb60186bd3167961843e9d834391d495f6a
saltchicken <john.eicher89@gmail.com>
2024-05-01 18:19:04 -0700

    Fix CWD

--- spritemaker/__init__.py
+++ spritemaker/__init__.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser(description="Spritemaker commandline")
     parser.add_argument('input', help='Input image')
     args = parser.parse_args()
-    os.chdir(os.getcwd())
+    cwd = os.getcwd()
+    print(cwd)
     spritemaker = Spritemaker(args.input)
     spritemaker.show()
